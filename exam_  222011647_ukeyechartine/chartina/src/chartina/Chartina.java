e66fb8762661cad303d6966daddc2a99e92c125f
Chartine222 <151142102+Chartine222@users.noreply.github.com>
2024-02-19 10:04:00 +0200

    Add files via upload

---
+++ exam_  222011647_ukeyechartine/chartina/src/chartina/Chartina.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package chartina;
+
+/**
+ *
+ * @author Chartine
+ */
+public class Chartina {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
